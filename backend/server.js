d91672996ccd9907e03a69d9703d7b9bafb1dd76
kolliDayakar77kolli <Kollidayakar77@gmail.com>
2024-05-03 16:24:03 +0530

    origin url updated

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,8 @@ const server = app.listen(PORT, ()=>{
 const io = require("socket.io")(server, {
     pingTimeout: 60000,
     cors: {
-      origin: "http://localhost:3000",
+      // origin: "http://localhost:3000",
+      origin: "https://realtime-mern-chat-app.onrender.com/",
       // credentials: true,
     },
   });
